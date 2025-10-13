b12933a8788e65c1f0f2d783efe0432106303787
duckyduckG <duckyduckg65@gmail.com>
2025-10-13 18:32:25 +0500

    Revert "qcom: clk: osm-sdm845: Set dvfs_possible_from_any_cpu from cpufreq"
    This reverts commit 5f7622fe40f3d8661baa1be5c968721001b70d34.

--- drivers/clk/qcom/clk-cpu-osm-sdm845.c
+++ drivers/clk/qcom/clk-cpu-osm-sdm845.c
@@ -799,7 +799,6 @@ static int osm_cpufreq_cpu_init(struct cpufreq_policy *policy)
 
 	policy->freq_table = table;
 	policy->driver_data = c;
-	policy->dvfs_possible_from_any_cpu = true;
 	cpumask_copy(policy->cpus, &c->related_cpus);
 	return 0;
 }
